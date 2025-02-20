7bfbae922e53a40a973c7c3c69105f54c62b9ac3
TaibkKurbanaliev <taibkurbanaliev@gmail.com>
2025-02-20 21:58:21 +0400

    create base html template and first models

--- portfolio/technology/models.py
+++ portfolio/technology/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Technology(models.Model): 
+    name = models.CharField(max_length=32, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+class Project(models.Model):
+    name = models.CharField(max_length=128)
+    link = models.CharField(max_length=512)
+    imdage = models.ImageField(upload_to='project_iamges')
+    description = models.TextField()
+    technology_owner = models.ForeignKey(to=Technology, on_delete=models.PROTECT)
